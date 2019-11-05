60e1ff247f3e1239635bd779650ec0f2f7eb166f
Markus Ahlstrand <markus@ahlstrand.es>
2019-11-05 15:34:46 +0100

    Updated siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,8 +22,8 @@ const users = [
 
 const siteConfig = {
   title: "Cloudproxy", // Title for your website.
-  tagline: "Cloudflare Api Management",
-  url: "https://cloudproxy.io", // Your website URL
+  tagline: "Api Management for edge",
+  url: "https://markusalhstrand.github.io/cloudproxy-www", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Ahlstrand Technology S.L.`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
